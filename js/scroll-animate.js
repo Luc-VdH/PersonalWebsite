7b1b59e2160e8f10d7ec04d900ba36b308600e75
Luc Van Den Handel <artemis@Lucs-MBP.lan>
2021-01-04 14:38:57 +0200

    Working animations

--- js/scroll-animate.js
+++ js/scroll-animate.js
@@ -1,3 +1,57 @@
+let top1 = window.pageYOffset + window.innerHeight,
+    isVisible1 = top1 > document.querySelector('#head1').offsetTop;
+let top2  = window.pageYOffset + window.innerHeight,
+    isVisible2 = top2 > document.querySelector('#head2').offsetTop;
+let top3  = window.pageYOffset + window.innerHeight,
+    isVisible3 = top3 > document.querySelector('#head3').offsetTop;
+let top4  = window.pageYOffset + window.innerHeight,
+    isVisible4 = top4 > document.querySelector('#head4').offsetTop;
+let top5  = window.pageYOffset + window.innerHeight,
+    isVisible5 = top5 > document.querySelector('#head5').offsetTop;
+let top6  = window.pageYOffset + window.innerHeight,
+    isVisible6 = top6 > document.querySelector('#head6').offsetTop;
+let top7  = window.pageYOffset + window.innerHeight,
+    isVisible7 = top7 > document.querySelector('#head7').offsetTop;
+let top8  = window.pageYOffset + window.innerHeight,
+    isVisible8 = top8 > document.querySelector('#head8').offsetTop;
+let top9  = window.pageYOffset + window.innerHeight,
+    isVisible9 = top9 > document.querySelector('#head9').offsetTop;
+let top10  = window.pageYOffset + window.innerHeight,
+    isVisible10 = top10 > document.querySelector('#head10').offsetTop;
+
+
+if (isVisible1) {
+    document.getElementById('head1').classList.add('pop-in');
+}
+if (isVisible2) {
+    document.getElementById('head2').classList.add('pop-in');
+}
+if (isVisible3) {
+    document.getElementById('head3').classList.add('pop-in');
+}
+if (isVisible4) {
+    document.getElementById('head4').classList.add('pop-in');
+}
+if (isVisible5) {
+    document.getElementById('head5').classList.add('pop-in');
+}
+if (isVisible6) {
+    document.getElementById('head6').classList.add('pop-in');
+}
+if (isVisible7) {
+    document.getElementById('head7').classList.add('pop-in');
+}
+if (isVisible8) {
+    document.getElementById('head8').classList.add('pop-in');
+}
+if (isVisible9) {
+    document.getElementById('head9').classList.add('pop-in');
+}
+if (isVisible10) {
+    document.getElementById('head10').classList.add('pop-in');
+}
+
+
 document.addEventListener('scroll', function (e) {
     let top1 = window.pageYOffset + window.innerHeight,
         isVisible1 = top1 > document.querySelector('#head1').offsetTop;
@@ -53,3 +107,59 @@ document.addEventListener('scroll', function (e) {
         }
         
     });
+
+document.addEventListener('scroll', function (e) {
+    let top1 = window.pageYOffset + window.innerHeight,
+        isVisible1 = top1 > document.querySelector('#text1').offsetTop;
+    let top2  = window.pageYOffset + window.innerHeight,
+        isVisible2 = top2 > document.querySelector('#text2').offsetTop;
+    let top3  = window.pageYOffset + window.innerHeight,
+        isVisible3 = top3 > document.querySelector('#text3').offsetTop;
+    let top4  = window.pageYOffset + window.innerHeight,
+        isVisible4 = top4 > document.querySelector('#text4').offsetTop;
+    let top5  = window.pageYOffset + window.innerHeight,
+        isVisible5 = top5 > document.querySelector('#text5').offsetTop;
+    let top6  = window.pageYOffset + window.innerHeight,
+        isVisible6 = top6 > document.querySelector('#text6').offsetTop;
+    let top7  = window.pageYOffset + window.innerHeight,
+        isVisible7 = top7 > document.querySelector('#text7').offsetTop;
+    let top8  = window.pageYOffset + window.innerHeight,
+        isVisible8 = top8 > document.querySelector('#text8').offsetTop;
+    let top9  = window.pageYOffset + window.innerHeight,
+        isVisible9 = top9 > document.querySelector('#text9').offsetTop;
+    let top10  = window.pageYOffset + window.innerHeight,
+        isVisible10 = top10 > document.querySelector('#text10').offsetTop;
+
+
+    if (isVisible1) {
+        document.getElementById('text1').classList.add('pop-in');
+    }
+    if (isVisible2) {
+        document.getElementById('text2').classList.add('pop-in');
+    }
+    if (isVisible3) {
+        document.getElementById('text3').classList.add('pop-in');
+    }
+    if (isVisible4) {
+        document.getElementById('text4').classList.add('pop-in');
+    }
+    if (isVisible5) {
+        document.getElementById('text5').classList.add('pop-in');
+    }
+    if (isVisible6) {
+        document.getElementById('text6').classList.add('pop-in');
+    }
+    if (isVisible7) {
+        document.getElementById('text7').classList.add('pop-in');
+    }
+    if (isVisible8) {
+        document.getElementById('text8').classList.add('pop-in');
+    }
+    if (isVisible9) {
+        document.getElementById('text9').classList.add('pop-in');
+    }
+    if (isVisible10) {
+        document.getElementById('text10').classList.add('pop-in');
+    }
+
+});
