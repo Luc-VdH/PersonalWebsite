7ad8066a154ae3e357151559f2e9b92ca94ac711
Luc-VdH <lucvdhandel@gmail.com>
2021-02-25 12:53:33 +0200

    Started work on new page for personal projects

--- js/scroll-animate.js
+++ js/scroll-animate.js
@@ -51,6 +51,60 @@ if (isVisible10) {
     document.getElementById('head10').classList.add('pop-in');
 }
 
+let top1 = window.pageYOffset + window.innerHeight,
+    isVisible1 = top1 > document.querySelector('#text1').offsetTop;
+let top2  = window.pageYOffset + window.innerHeight,
+    isVisible2 = top2 > document.querySelector('#text2').offsetTop;
+let top3  = window.pageYOffset + window.innerHeight,
+    isVisible3 = top3 > document.querySelector('#text3').offsetTop;
+let top4  = window.pageYOffset + window.innerHeight,
+    isVisible4 = top4 > document.querySelector('#text4').offsetTop;
+let top5  = window.pageYOffset + window.innerHeight,
+    isVisible5 = top5 > document.querySelector('#text5').offsetTop;
+let top6  = window.pageYOffset + window.innerHeight,
+    isVisible6 = top6 > document.querySelector('#text6').offsetTop;
+let top7  = window.pageYOffset + window.innerHeight,
+    isVisible7 = top7 > document.querySelector('#text7').offsetTop;
+let top8  = window.pageYOffset + window.innerHeight,
+    isVisible8 = top8 > document.querySelector('#text8').offsetTop;
+let top9  = window.pageYOffset + window.innerHeight,
+    isVisible9 = top9 > document.querySelector('#text9').offsetTop;
+let top10  = window.pageYOffset + window.innerHeight,
+    isVisible10 = top10 > document.querySelector('#text10').offsetTop;
+
+
+if (isVisible1) {
+    document.getElementById('text1').classList.add('pop-in');
+}
+if (isVisible2) {
+    document.getElementById('text2').classList.add('pop-in');
+}
+if (isVisible3) {
+    document.getElementById('text3').classList.add('pop-in');
+}
+if (isVisible4) {
+    document.getElementById('text4').classList.add('pop-in');
+}
+if (isVisible5) {
+    document.getElementById('text5').classList.add('pop-in');
+}
+if (isVisible6) {
+    document.getElementById('text6').classList.add('pop-in');
+}
+if (isVisible7) {
+    document.getElementById('text7').classList.add('pop-in');
+}
+if (isVisible8) {
+    document.getElementById('text8').classList.add('pop-in');
+}
+if (isVisible9) {
+    document.getElementById('text9').classList.add('pop-in');
+}
+if (isVisible10) {
+    document.getElementById('text10').classList.add('pop-in');
+}
+
+
 
 document.addEventListener('scroll', function (e) {
     let top1 = window.pageYOffset + window.innerHeight,
---
+++ js/title-height.js
@@ -0,0 +1,4 @@
+var words = document.getElementById("head1").style.height;
+var box = document.getElementById("start");
+
+box.style.height = words;
